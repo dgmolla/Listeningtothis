ed67ca6db25f11ba1cb497862718a48597ea75ba
Dawit Molla <dgmolla@gmail.com>
2020-09-16 16:12:23 -0500

    init server

---
+++ server.js
@@ -0,0 +1,10 @@
+/* Load HTTP Library */
+var http = require("http");
+
+/* Create HTTP server for resposne handling */
+
+http.createServer(function(request, response) {
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.write("Hello World");
+    response.end();
+  }).listen(8888);
