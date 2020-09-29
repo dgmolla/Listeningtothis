a41d7d8f6bbf2b4fa2be9dbb7faee0e37ebb5b34
Dawit Molla <dgmolla@gmail.com>
2020-09-29 12:07:22 -0500

    Integrated react frontend, modularized

--- README.md
+++
@@ -1,8 +0,0 @@
-# MyJam
-
-
-Find your Jam! MyJam lets you easily see the music and artists you've been bumping on Spotify the most.
-
-Notes:
-
-1.0.0: Struggling with authentication, working with different API to wrap my head around Node/Express and EJS
---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+const App = () => (
+  <div className="App">
+    <a href='http://localhost:8888'>
+      <button>Log in with Spotify</button>
+    </a>
+  </div>
+);
+
+export default App;
